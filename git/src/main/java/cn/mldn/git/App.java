c33c412c79acb734884700284aa8a44b92e9d3bc
chenchao <1044316902@qq.com>
2018-07-08 14:45:20 +0800

    1

--- git/src/main/java/cn/mldn/git/App.java
+++ git/src/main/java/cn/mldn/git/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worl1d!111111" );
+        System.out.println( "Hello Worl1d1!111111" );
     }
 }
